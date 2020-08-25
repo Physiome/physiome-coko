6b83de0a55b92b30da0d3044f7b55ec025dce11b
Tommy Yu <tommy.yu@auckland.ac.nz>
2020-08-25 17:24:51 +1200

    [Physiome] Email to curator on new submission
    - Also send one to be sure that the curators know about it.

--- packages/physiome-workflow-tasks/src/task-handlers/task-email-initial-submission.js
+++ packages/physiome-workflow-tasks/src/task-handlers/task-email-initial-submission.js
@@ -1,5 +1,7 @@
 const TaskSendEmail = require('./util-task-send-email');
 const logger = require('workflow-utils/logger-with-prefix')('PhysiomeWorkflowTasks/Email-InitialSubmission');
+const config = require('config');
+const EditorsMailingListAddress = config.get('workflow-send-email.editorsMailingListAddress');
 
 class TaskSendInitialSubmissionEmail extends TaskSendEmail {
 
@@ -12,10 +14,38 @@ class TaskSendInitialSubmissionEmail extends TaskSendEmail {
     }
 }
 
+class TaskSendInitialSubmissionListEmail extends TaskSendEmail {
+
+    constructor(logger) {
+        super('manuscript-submitted-editor-list', logger);
+    }
+
+    async formatEmailSubject(submission) {
+        return `submission ${submission.manuscriptId} ready for review`;
+    }
+
+    async submissionToRecipient(submission) {
+        return {
+            displayName: 'Physiome Editors',
+            email: EditorsMailingListAddress
+        };
+    }
+
+    skipTaskWithoutSendingEmail() {
+        return !EditorsMailingListAddress;
+    }
+}
+
 module.exports = function _setupEmailInitialSubmissionTask(client) {
 
     const externalTaskName = 'initial-submission-email';
-    const task = new TaskSendInitialSubmissionEmail(logger);
+    const emailtask1 = new TaskSendInitialSubmissionEmail(logger);
+    const emailtask2 = new TaskSendInitialSubmissionListEmail(logger);
 
-    task.configure(client, externalTaskName);
+    client.subscribe(externalTaskName, async ({ task, taskService }) => {
+        return Promise.all([
+            emailtask1.processTask(task, taskService),
+            emailtask2.processTask(task, taskService)
+        ]);
+    });
 };
